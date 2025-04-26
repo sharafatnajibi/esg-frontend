68a805a601a51e47d585d503ab88f9f347521086
Sharafat Najibi <sharafat.najibi@outlook.com>
2025-04-25 21:19:33 -0700

    Initial frontend commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
-import SelectFieldPage from './pages/SelectFieldPage'; // NEW
+import SelectFieldPage from './pages/SelectFieldPage';
 import FormPage from './pages/FormPage';
 import ResultPage from './pages/ResultPage';
 
@@ -13,12 +13,11 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/select-field" element={<SelectFieldPage />} /> {/* NEW */}
+        <Route path="/select-field" element={<SelectFieldPage />} />
         <Route path="/form" element={<FormPage />} />
         <Route path="/result" element={<ResultPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
